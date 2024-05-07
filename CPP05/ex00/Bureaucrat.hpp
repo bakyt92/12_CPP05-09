d9a90c5f4a040fa938027eb87440c199c5d662f0
ufitzhug <ufitzhug@student.42.fr>
2024-05-07 17:00:54 +0200

    ex00

--- CPP05/ex00/Bureaucrat.hpp
+++ CPP05/ex00/Bureaucrat.hpp
@@ -6,15 +6,30 @@
 /*   By: ufitzhug <ufitzhug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 16:43:47 by ufitzhug          #+#    #+#             */
-/*   Updated: 2024/05/07 16:46:35 by ufitzhug         ###   ########.fr       */
+/*   Updated: 2024/05/07 17:00:52 by ufitzhug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "iostream"
-
-#ifndef
-#define 
+#include <iostream>
+#include <string>
 
 
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+class Bureaucrat
+{
+private:
+	const std::string _name;
+	int _grade;
+public:
+	Bureaucrat(/* args */);
+	Bureaucrat(const Bureaucrat &src);
+	Bureaucrat & operator= (const Bureaucrat &rhs);
+	const std::string getName() const;
+	setGrade(int g);
+	int getGrade() const;
+	~Bureaucrat();
+};
 
 #endif
