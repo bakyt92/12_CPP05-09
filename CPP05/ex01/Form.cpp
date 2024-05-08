6f2a4772f679ad0c4c1ce77853b267b8a6dc2477
bakyt92 <bakyt92@mail.ru>
2024-05-08 13:37:34 +0300

    ex01 - CPP05

--- CPP05/ex01/Form.cpp
+++ CPP05/ex01/Form.cpp
@@ -6,7 +6,7 @@
 /*   By: ufitzhug <ufitzhug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 12:14:53 by ufitzhug          #+#    #+#             */
-/*   Updated: 2024/05/08 13:27:02 by ufitzhug         ###   ########.fr       */
+/*   Updated: 2024/05/08 13:36:52 by ufitzhug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,17 @@ Form::Form (std::string name, int gradeToSign, int gradeToExecute) : _name(name)
     this->_isSigned = 0;
 }
 
-Form::Form(const Form &src)
+Form::Form(const Form &src) : _name(src._name), _isSigned(src._isSigned), _gradeToSign(src._gradeToSign), _gradeToExecute(src._gradeToExecute)
 {
-    
+    std::cout << "Form copy constructor is called." << std::endl;
+    *this = src;
+}
+
+Form& Form::operator= (const Form &rhs)
+{
+    if (this != &rhs)
+        this->_isSigned = rhs._isSigned;
+    return *this;
 }
 
 
