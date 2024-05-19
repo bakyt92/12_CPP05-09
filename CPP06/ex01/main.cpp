cecbc0065239170e74f925ee98eca5f5d6821f67
bakyt92 <bakyt92@mail.ru>
2024-05-19 22:56:18 +0300

    CPP06 - ex00

--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ufitzhug <ufitzhug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 02:55:56 by ufitzhug          #+#    #+#             */
-/*   Updated: 2024/05/19 03:08:42 by ufitzhug         ###   ########.fr       */
+/*   Updated: 2024/05/19 22:49:35 by ufitzhug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int main ()
 {
+    /* Serialize */
     Data* fond = new Data;
     fond->i = 15;
     fond->str = "Hello students of 42";
@@ -22,5 +23,17 @@ int main ()
     raw = Serializer::serialize(fond);
     std::cout << "Ater: " << fond << " raw info: " << raw << std::endl;
     delete fond;
+
+    /* De-serialize */
+    Data* oldData = new Data;
+    oldData->i = 15;
+    oldData->str = "Hello World";
+    Data *newData;
+    std::cout << "Before pointer: " << oldData << std::endl;
+    std::cout << "Before str data: " << oldData->str << std::endl;
+    // std::cout << "Before str data for NewData: " << newData->str << std::endl;
+    newData = Serializer::deserialize(Serializer::serialize(oldData));
+    std::cout << "After pointer: " << newData << std::endl;
+    std::cout << "After str data: " << newData->str << std::endl;
     return 0;
 }
