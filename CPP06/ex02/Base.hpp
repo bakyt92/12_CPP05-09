2b9543c17a46d000ab3fd20cca10835cb6e2576e
bakyt92 <bakyt92@mail.ru>
2024-05-19 23:08:41 +0300

    CPP06 - ex02

--- CPP06/ex02/Base.hpp
+++ CPP06/ex02/Base.hpp
@@ -6,7 +6,25 @@
 /*   By: ufitzhug <ufitzhug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 22:55:57 by ufitzhug          #+#    #+#             */
-/*   Updated: 2024/05/19 22:55:58 by ufitzhug         ###   ########.fr       */
+/*   Updated: 2024/05/19 23:05:52 by ufitzhug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BASE_HPP
+#define BASE_HPP
+
+class Base
+{
+private:
+    /* data */
+public:
+    Base(/* args */);
+    ~Base();
+    Base * generate(void);
+    void identify(Base* p);
+    void identify(Base& p);
+};
+
+
+
+#endif
