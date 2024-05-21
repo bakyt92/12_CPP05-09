109fa5715578d0fc0c4e28193dcc2f2c81a1885a
ufitzhug <ufitzhug@student.42.fr>
2024-05-21 17:48:37 +0200

    CPP07 - ex02

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -6,21 +6,60 @@
 /*   By: ufitzhug <ufitzhug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 16:47:23 by ufitzhug          #+#    #+#             */
-/*   Updated: 2024/05/21 17:07:27 by ufitzhug         ###   ########.fr       */
+/*   Updated: 2024/05/21 17:48:29 by ufitzhug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <iostream>
+
+template <typename T>
 class Array
 {
 private:
-	/* data */
+	T* _data;
+	int _size;
 public:
-	Array();
-	Array(unsigned int n);
-	~Array();
+	Array() : _data(new T), _size(0) {
+		std::cout << "Constructor: Array created with size of " << this->_size << std::endl;
+	}
+	
+	Array(unsigned int n): _data(new T[n]), _size(n)  {
+		std::cout << "Constructor: Array created with size of " << this->_size << std::endl;
+	}
+	
+	Array(const & src): _data(new T[src._size]), _size(src._size)
+	{
+		std::cout << "Copy constructor for class Array is called" << std::endl;
+		int i = 0;
+		while (i < this->_size)
+		{
+			src._data[i] = this->data;
+			i++;
+		}
+	}
+
+	Array& operator=(Array &rhs const)
+	{
+		std::cout << "Copy assignment operator for class Array is called" << std::endl;
+		if (rhs != this)
+		{
+			this->_size = rhs._size;
+			int i = 0;
+			while (i < this->_size)
+			{
+				src._data[i] = this->data;
+				i++;
+			}
+		}
+		return *this;
+	}
+	
+	~Array() {
+		delete [] _data;
+	};
 };
 
 #endif
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -6,13 +6,60 @@
 /*   By: ufitzhug <ufitzhug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 16:46:51 by ufitzhug          #+#    #+#             */
-/*   Updated: 2024/05/21 16:48:04 by ufitzhug         ###   ########.fr       */
+/*   Updated: 2024/05/21 17:12:51 by ufitzhug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Array.hpp"
 
-int main()
+#define MAX_VAL 750
+int main(int, char**)
 {
-	return 0;
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+    return 0;
 }
