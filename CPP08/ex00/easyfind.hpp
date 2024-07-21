362e667677ddfa0f39d35e1a147fe10750318eab
ufitzhug <ufitzhug@student.42.fr>
2024-07-21 23:33:26 +0200

    423432

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -6,7 +6,34 @@
 /*   By: ufitzhug <ufitzhug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 19:41:38 by ufitzhug          #+#    #+#             */
-/*   Updated: 2024/07/04 19:47:13 by ufitzhug         ###   ########.fr       */
+/*   Updated: 2024/07/21 23:33:26 by ufitzhug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASYFIND
+#define EASYFIND
+
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+template <typename T> bool easyfind (T& container, int x)
+{
+	if (std::find (container.begin(), container.end(), x) == container.end())
+	{
+		throw EasyException();
+		return false;
+	}
+	else 
+		return true;
+}
+
+class EasyException : public std::exception {
+	public:
+		const char *what() const throw () {
+			return "easyfind: exception: integer is not found in the container";
+		}
+};
+
+#endif
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ufitzhug <ufitzhug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 19:41:42 by ufitzhug          #+#    #+#             */
-/*   Updated: 2024/07/04 19:47:39 by ufitzhug         ###   ########.fr       */
+/*   Updated: 2024/07/21 23:33:26 by ufitzhug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,33 @@
 
 int main ()
 {
+	std::vector<int> vec;
+	for (int i = 0; i < 10; i++)
+	{
+		vec.push_back(i);
+	}
+	std::cout << "vec: ";
+	for (std::vector<int>::iterator iter = vec.begin(), end = vec.end(); iter != end; iter++)
+	{
+		std::cout << *iter << " ";
+	}
+	std::cout << std::endl;
+
+	try {
+		easyfind(vec, 2);
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	try {
+		easyfind(vec, 22);
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
 	return 0;
 }
