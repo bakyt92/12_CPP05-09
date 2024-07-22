f1852c4b639f9d9ae11b526051f1fe47b77a4939
ufitzhug <ufitzhug@student.42.fr>
2024-07-22 18:59:47 +0200

    ex01

--- CPP08/ex01/Span.cpp
+++ CPP08/ex01/Span.cpp
@@ -6,7 +6,33 @@
 /*   By: ufitzhug <ufitzhug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 17:53:31 by ufitzhug          #+#    #+#             */
-/*   Updated: 2024/07/22 17:53:32 by ufitzhug         ###   ########.fr       */
+/*   Updated: 2024/07/22 18:58:55 by ufitzhug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Span.hpp"
+
+Span::Span(unsigned int i): _i(i)
+{
+	std::cout << "Span contructor is called." << std::endl;
+}
+
+Span::Span(const Span &src)
+{
+	this->_i = src._i;
+	this->_list = src._list;
+	std::cout << "Copy Span contructor is called." << std::endl;
+	*this = src;
+}
+
+Span& Span::operator=(const Span &rhs)
+{
+	std::cout << "Copy assignment operator called." << std::endl;
+	if (this != &rhs)
+	{
+		this->_i = rhs._i;
+		this->_list = rhs._list;
+	}
+	return *this;
+}
 
--- CPP08/ex01/Span.hpp
+++ CPP08/ex01/Span.hpp
@@ -6,7 +6,32 @@
 /*   By: ufitzhug <ufitzhug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 17:53:35 by ufitzhug          #+#    #+#             */
-/*   Updated: 2024/07/22 17:53:50 by ufitzhug         ###   ########.fr       */
+/*   Updated: 2024/07/22 18:56:48 by ufitzhug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SPAN
+#define SPAN
+
+#include <iostream>
+#include <algorithm>
+#include <list>
+#include <exception>
+
+class Span
+{
+private:
+	std::list<int> _list;
+	unsigned int _i;
+public:
+	Span(unsigned int i);
+	Span(const Span &src);
+	Span& operator=(const Span &rhs);
+	~Span();
+
+	void addnumber(int x);
+	int shortestSpan();
+	int longestSpan();
+};
+
+#endif
