833d5c52a0e00c05504219b8904132d5a7e152c1
ufitzhug <ufitzhug@student.42.fr>
2024-07-22 19:56:25 +0200

    ex01 - CPP08

--- CPP08/ex01/Span.cpp
+++ CPP08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: ufitzhug <ufitzhug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 17:53:31 by ufitzhug          #+#    #+#             */
-/*   Updated: 2024/07/22 18:58:55 by ufitzhug         ###   ########.fr       */
+/*   Updated: 2024/07/22 19:55:11 by ufitzhug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,3 +36,69 @@ Span& Span::operator=(const Span &rhs)
 	return *this;
 }
 
+Span::~Span()
+{
+	std::cout << "Span destructor is called." << std::endl;
+}
+
+class QuantityException : public std::exception {
+	public:
+		const char* what() const throw() {
+        	return "Max size of list is reached. Impossible to add more numbers.";
+    }
+};
+
+class NotSufficientException : public std::exception {
+	public:
+		const char* what() const throw() {
+        	return "Less than 2 elements. Not possible to find span.";
+    }
+};
+
+void Span::addnumber(int x)
+{
+	if (_list.size() > _i)
+		throw QuantityException();
+	std::list<int>::iterator it;
+	it = _list.begin();
+	_list.insert(it, x);
+	return ;
+}
+
+int Span::shortestSpan()
+{
+	if (_list.size() < 2)
+		throw NotSufficientException();
+	std::list<int>::iterator it;
+	std::list<int>::iterator it2;
+	it = _list.begin();
+	it2 = _list.begin();
+	++it2;
+	int res = (*it2 - *it);
+	while (it2 != _list.end())
+	{
+		res = std::min(res, (*it2 - *it));
+		++it2;
+		++it;
+	}
+	return res;
+}
+
+int Span::longestSpan()
+{
+	if (_list.size() < 2)
+		throw NotSufficientException();
+	std::list<int>::iterator it;
+	std::list<int>::iterator it2;
+	it = _list.begin();
+	it2 = _list.begin();
+	++it2;
+	int res = (*it2 - *it);
+	while (it2 != _list.end())
+	{
+		res = std::max(res, (*it2 - *it));
+		++it2;
+		++it;
+	}
+	return res;
+}
