74a61dd2f7a5bc2268e5f2ba7919a73f91d51fd9
ufitzhug <ufitzhug@student.42.fr>
2024-08-25 16:08:04 +0200

    CPP09-ex02

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: ufitzhug <ufitzhug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 22:59:21 by ufitzhug          #+#    #+#             */
-/*   Updated: 2024/08/25 16:07:17 by ufitzhug         ###   ########.fr       */
+/*   Updated: 2024/08/25 16:07:59 by ufitzhug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ template<typename Container>
 PmergeMe<Container>::PmergeMe (char **input)
 {
 	std::cout << "Constructor for instance PmergeMe is called" << std::endl;
-	
 }
 
 template<typename Container>
